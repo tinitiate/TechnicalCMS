6cf1ebb24bee470f6f86d345ef56848fb949cf8b
tinitiate.com <tinitiate@users.noreply.github.com>
2017-12-04 21:40:13 -0500

    1

---
+++ common/js/ti.js
@@ -0,0 +1,105 @@
+$("#globalnav").html('<!-- Navbar Start --> \
+<div class="navbar navbar-inverse navbar-fixed-top"> \
+    <div class="navbar-header"> \
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"> \
+        <span class="icon-bar"></span> \
+        <span class="icon-bar"></span> \
+        <span class="icon-bar"></span> \
+        </button> \
+    </div> \
+    <div class="collapse navbar-collapse"> \
+        <ul class="nav navbar-nav"> \
+        <!-- TopBar Content Start --> \
+        <li class="active"><a href="http://www.tinitiate.com">TINITIATE.COM</a></li> \
+        <li class="dropdown"> \
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-terminal" aria-hidden="true"></i> tutorials \
+            <span class="caret"></span></a> \
+            <ul class="dropdown-menu"> \
+            <li><a href="programming.html">programming</a></li> \
+            <li><a href="databases.html">databases</a></li> \
+            <li><a href="data-formats.html">data formats</a></li> \
+            <li><a href="web-development.html">web development</a></li> \
+            </ul> \
+        </li> \
+        <li class="dropdown"> \
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-desktop" aria-hidden="true"></i> Syntax Board \
+            <span class="caret"></span></a> \
+            <ul class="dropdown-menu"> \
+            <li><a href="oracle-syntax-board.html">Oracle</a></li> \
+            <li><a href="mysql-syntax-board.html">MySQL</a></li> \
+            <li><a href="mssqlserver-syntax-board.html">MS SQL Server</a></li> \
+            </ul> \
+        </li> \
+        <li class="dropdown"> \
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-flask" aria-hidden="true"></i> labs \
+            <span class="caret"></span></a> \
+            <ul class="dropdown-menu"> \
+            <li><a href="#">Java interview questions</a></li> \
+            <li><a href="#">Oracle interview questions</a></li> \
+            <li><a href="#">MySQL interview questions</a></li> \
+            <li><a href="#">MS SQL Server interview questions</a></li> \
+            <li><a href="#">Python interview questions</a></li> \
+            <li><a href="#">Python Django interview questions</a></li> \
+            <li><a href="#">Python Flask interview questions</a></li> \
+            </ul> \
+        </li> \
+        <li class="dropdown"> \
+            <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="fa fa-cogs" aria-hidden="true"></i> Products and Services \
+            <span class="caret"></span></a> \
+            <ul class="dropdown-menu"> \
+            <li><a href="remote-training.html">Remote Training</a></li> \
+            <li><a href="http://www.github.com/tinitiate/TechnicalCMS">Technical CMS</a></li> \
+            </ul> \
+        </li> \
+        </ul> \
+        <ul class="nav navbar-nav navbar-right"> \
+        <li> \
+            <style type="text/css">.search-box {width: 500px}</style><div class="search-box" style="color:black";><gcse:searchbox-only></gcse:searchbox-only></div> \
+        </li> \
+        <!-- TopBar Content End --> \
+        </ul> \
+    </div> \
+</div>');
+
+
+$("#globalfooter").html('<!-- Footer --> \
+    <footer class="footer"  style="background-color:black; color:white;"> \
+        <div class="container"> \
+          <div class="row"> \
+            <br><br> \
+            <div class="col-md-12"> \
+                <div class="col-md-4"> \
+                <ul class="list-unstyled" style="color:white;"> \
+                <li><i class="fa fa-github" aria-hidden="true"></i> <a href="https://github.com/tinitiate/">github</a></li> \
+                <li><i class="fa fa-bitbucket" aria-hidden="true"></i> <a href="https://bitbucket.org/tinitiate/">bitbucket</a></li> \
+                <li><i class="fa fa-gitlab" aria-hidden="true"></i> <a href="https://gitlab.com/tinitiate/">gitlab</a></li> \
+                </ul> \
+                </div> \
+                <div class="col-md-4"> \
+                <ul class="list-unstyled" style="color:white;"> \
+                <li><a href="https://github.com/tinitiate/TechnicalCMS">Technical CMS</a></li> \
+                <li><a href="/syntax-board/index.html">Syntax Board</a></li> \
+                </ul> \
+                </div> \
+                <div class="col-md-4"> \
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> \
+                <!-- FooterAdA1 --> \
+                <ins class="adsbygoogle" \
+                     style="display:block" \
+                     data-ad-client="ca-pub-1094859208796082" \
+                     data-ad-slot="1892435980" \
+                     data-ad-format="auto"></ins> \
+                <script> \
+                (adsbygoogle = window.adsbygoogle || []).push({}); \
+                </script> \
+                </div> \
+            </div> \
+            <div class="col-md-12 text-center"> <br> \
+            <a href="http://www.tinitiate.com">home</a> | <a href="contactus.html">contact us</a> | <a href="aboutus.html">about us</a> <br> \
+            </div> <br> \
+            <div class="col-md-12"> \
+&copy; 2017 tinitiate.com , All rights reserved. While using this site, you agree to have read and accepted our <a href="tos.html" style="color:yellow;">Terms of Service</a> and <a href="tos.html" style="color:yellow;">Privacy Policy</a>. <br><br> \
+            </div> \
+          </div> \
+        </div> \
+    </footer>');
---
+++ common/js/tinitiate.js
@@ -0,0 +1,95 @@
+$(document).ready(function() {
+  $('[data-toggle=offcanvas]').click(function() {
+    $('.row-offcanvas').toggleClass('active');
+  });
+});
+
+$("#socialbuttons").html('\
+<div class="panel panel-default"> \
+    <div class="panel-body"> \
+    <div class="a2a_kit a2a_kit_size_16 a2a_default_style"> \
+    <a class="a2a_dd" href="https://www.addtoany.com/share"></a> \
+    <a class="a2a_button_linkedin"></a> \
+    <a class="a2a_button_facebook"></a> \
+    <a class="a2a_button_twitter"></a> \
+    <a class="a2a_button_google_gmail"></a> \
+    <a class="a2a_button_google_plus"></a> \
+    <a class="a2a_button_google_bookmarks"></a> \
+    <a class="a2a_button_whatsapp"></a> \
+    <a class="a2a_button_pinterest"></a> \
+    <a class="a2a_button_email"></a> \
+    <a class="a2a_button_delicious"></a> \
+    <a class="a2a_button_digg"></a> \
+    <a class="a2a_button_evernote"></a> \
+    <a class="a2a_button_pocket"></a> \
+    <a class="a2a_button_skype"></a> \
+    <a class="a2a_button_line"></a> \
+    <a class="a2a_button_flipboard"></a> \
+    <a class="a2a_button_facebook_messenger"></a> \
+    <a class="a2a_button_reddit"></a> \
+    <a class="a2a_button_tumblr"></a> \
+    <a class="a2a_button_baidu"></a> \
+    <a class="a2a_button_wechat"></a> \
+    <a class="a2a_button_telegram"></a> \
+    </div> \
+    </div>\
+</div>');
+
+$("#googleanalytics").html("<script> \
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ \
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), \
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) \
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); \
+ \
+  ga('create', 'UA-56563407-1', 'auto'); \
+  ga('send', 'pageview'); \
+ \
+</script>");
+
+$("#respad1").html(' \
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> \
+<ins class="adsbygoogle" \
+     style="display:block" \
+     data-ad-client="ca-pub-1094859208796082" \
+     data-ad-slot="3815951856" \
+     data-ad-format="auto"></ins> \
+<script> \
+(adsbygoogle = window.adsbygoogle || []).push({}); \
+</script>');
+
+
+$("#respad2").html(' \
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> \
+<ins class="adsbygoogle" \
+     style="display:block" \
+     data-ad-client="ca-pub-1094859208796082" \
+     data-ad-slot="5292685054" \
+     data-ad-format="auto"></ins> \
+<script> \
+(adsbygoogle = window.adsbygoogle || []).push({}); \
+</script>');
+
+
+$("#inPage1").html(' \
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> \
+<ins class="adsbygoogle" \
+     style="display:block; text-align:center;" \
+     data-ad-layout="in-article" \
+     data-ad-format="fluid" \
+     data-ad-client="ca-pub-1094859208796082" \
+     data-ad-slot="9760787953"></ins> \
+<script> \
+     (adsbygoogle = window.adsbygoogle || []).push({}); \
+</script>');
+
+$("#FooterAd").html(' \
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> \
+<!-- FooterAdA1 --> \
+<ins class="adsbygoogle" \
+     style="display:block" \
+     data-ad-client="ca-pub-1094859208796082" \
+     data-ad-slot="1892435980" \
+     data-ad-format="auto"></ins> \
+<script> \
+(adsbygoogle = window.adsbygoogle || []).push({}); \
+</script>');
